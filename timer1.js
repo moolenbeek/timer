766430494f74e141dee5edb135b41e8a8ecd1730
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-30 20:06:25 +0000

    initial commit

---
+++ timer1.js
@@ -0,0 +1,10 @@
+var args = process.argv.slice(2);
+
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i]) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, 1000 * i);
+    }
+  }
